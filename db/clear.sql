160bc62364dea20973a81fb8f9d6292dc5c663fc
Kyle Myerson <Kyle Myerson>
2017-04-13 23:20:06 -0400

    fixed clear.sql

--- db/clear.sql
+++ db/clear.sql
@@ -1,3 +1,17 @@
+/$# /bin/csh -f
+
+set mysqlargs = "-h localhost -u group12 -pgroup12_pw"
+set db = group12_db
+
+
+mysql $mysqlargs << EOF
+DROP DATABASE group12_db;
+
+CREATE DATABASE group12_db;
+USE group12_db;
+
+
+
 DROP TABLE IF EXISTS Memos;
 DROP TABLE IF EXISTS Contributions;
 DROP TABLE IF EXISTS Profile;
